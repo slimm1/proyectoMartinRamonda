4e3e97d99cccba472276ee8a6c02664ba0b25aad
Vespertino <Vespertino@C04PC20.informatica.comercio>
2023-02-24 18:36:34 +0100

    Primer commit, creación de getter hobby

---
+++ proyectoMartinRamonda/src/proyectoMartinRamonda/MartinRamondaCocina.java
@@ -0,0 +1,18 @@
+package proyectoMartinRamonda;
+
+public class MartinRamondaCocina {
+	
+	String hobby, plato;
+	int numIngredientes;
+	public MartinRamondaCocina(String hobby, String plato, int numIngredientes) {
+		this.hobby=hobby;
+		this.plato=plato;
+		this.numIngredientes=numIngredientes;
+	}
+	
+	public String getHobby() {
+		return hobby;
+	}
+	
+	
+}
